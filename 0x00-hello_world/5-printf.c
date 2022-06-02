52a172e2cd0dd4c1d3d4f43b71e3c4116cd182dc
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-02 03:26:59 -0700

    q5 correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
- * main: entry file
- *
- * Return: 0 - success
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
