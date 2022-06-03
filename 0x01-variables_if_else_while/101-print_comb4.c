5fad2684a7f06384a2dd96a87242693f6ca5fac7
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-03 05:35:13 -0700

    q1 and q2 avanced inital sub

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 for success
+ */
+int main(void)
+{
+	return 0;
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - always zero for success
+ */
+int main(void)
+{
+	return 0;
+}
