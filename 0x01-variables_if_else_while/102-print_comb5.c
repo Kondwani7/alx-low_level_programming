64aebe0932d89728e93aee42a06ef2bb26c0feeb
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-03 05:57:50 -0700

    q2 advanced sub

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,9 +2,29 @@
 /**
  * main - Entry point
  *
- * Return - always zero for success
+ * Return: always zero for success
  */
 int main(void)
 {
-	return 0;
+	int first_comb;
+	int second_comb;
+
+	for (first_comb = 0; first_comb < 100; first_comb++)
+	{
+		for (second_comb = first_comb + 1; second_comb < 100; second_comb++)
+		{
+			putchar(first_comb / 10 + '0');
+			putchar(second_comb % 10 + '0');
+
+			putchar(' ');
+
+			if (first_comb == 98 && second_comb == 99)
+			{
+				break;
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
