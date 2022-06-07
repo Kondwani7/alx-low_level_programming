e7a5b9e7fb23e34c3b05b4194a2824d881a90841
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-07 04:28:47 -0700

    q6 sub

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - aboslute value of an integar
+ * @n: the integar
+ * Return: Always 0 for success
+ */
+int _abs(int n)
+{
+	if (n < O)
+	{
+		return (n * (-1));
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n);
+	}
+}
