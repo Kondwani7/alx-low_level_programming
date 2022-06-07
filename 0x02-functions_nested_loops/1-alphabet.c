d5a702dee618b5a3be468a73111adc9a9b9aceab
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-07 03:18:29 -0700

    q1 sub

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point: print 'alphabet'
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	_putchar('a');
+	_putchar('l');
+	_putchar('p');
+	_putchar('h');
+	_putchar('a');
+	_putchar('b');
+	_putchar('e');
+	_putchar('t');
+	_putchar('\n');
+
+	return (0);
+}
