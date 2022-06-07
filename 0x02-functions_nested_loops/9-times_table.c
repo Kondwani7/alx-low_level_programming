e17f0c9edf89198f6a8f9ce06c8a18dde9a78063
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-07 05:42:49 -0700

    q9 sub

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,39 +9,26 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
-	for (a = 0; a <= 9; a++)
+
+	for (a  = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar (48);
+		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
-		       if ((c / 10) ==  0)
-		       {
-			       if (b ==0)
-			       {
-				       _putchar ('0');
-			       }
-			       if (b != 0)
-			       {
-				       _putchar (' ');
-				       _putchar ((c % 10) + '0');
-			       }
-			       if (b < 9)
-			       {
-				       _putchar(',');
-				       _puthcar(' ');
-			       }
-		       }
-		       else
-		       {
-			       _putchar ((c / 10) + '0');
-			       _putchar ((c / 10) + '0');
-			       if (b < 9)
-			       {
-				       _putchar(',');
-				       _putchar(' ');
-			       }
-		       }
+			_putchar (44);
+			_putchar (32);
+			if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_puthcar((c % 10) + 48);
+			}
 		}
-		_putchar ('\n');
+	_putchar('\n');
 	}
 }
