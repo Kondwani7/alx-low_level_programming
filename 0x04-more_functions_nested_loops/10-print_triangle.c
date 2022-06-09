7c04b6a9895521c373aebbc496c74dbec2a5a02a
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-09 07:06:58 -0700

    q10 correction

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,22 +6,16 @@
  */
 void print_triangle(int size)
 {
-	int r, c, i;
+	int row, column, k;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-		for (r = 0; r < size; r++)
-		{
-			for (c = size - r; c > 1; c--)
-			{
-				_putchar(' ');
-			}
-			for (i = r + c; i >= 1; i--)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+	for (row = 0; row < size; row++)
+	{
+		for (column = size - row; column > 1; column--)
+			_putchar(' ');
+		for (k = row + column; k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
 	}
 }
