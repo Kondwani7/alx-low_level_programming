b56dc7118c75c51c99a7920ecff1b6b1b8de243f
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-22 03:30:27 -0700

    q6 sub

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * eval_num - our recursion func
+ * @num: integer
+ * @iter: number to iterate
+ * Return: return 1 or 0
+ */
+int eval_num(int num, int iter)
+{
+	if (iter == num - 1)
+	{
+		return (1);
+	}
+	else if (num % iter == 0)
+	{
+		return (0);
+	}
+	else if (num % iter != 0)
+	{
+		return (eval_num(num, iter + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * is_prime_number - evaluate if a integer is a prime number or not
+ * @n: integer
+ * Return: the prime number if valid
+ */
+int is_prime_number(int n)
+{
+	int iter;
+
+	iter = 2;
+
+	if (num < 2)
+	{
+		return (0);
+	}
+	else if (num == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (eval_num(n, iter));
+	}
+}
