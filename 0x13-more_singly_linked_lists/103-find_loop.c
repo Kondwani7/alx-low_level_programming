a5b42538764d809c58b32f3f51cebf14a4d20cbf
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-19 10:20:50 -0700

    q3 advanced floyd's cycle

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_lisint_loop - floyd's cycle tortise and hare
+ * @head: head of linked lsit
+ *
+ * Return: node where loop occcurs if found
+ */
+
+listint_t *find_lisint_loop(lisint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+
+	return (NULL);
+}
