e60434d89eef8fbb208b12d71d89659997f0e4a7
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-21 15:13:58 -0700

    q4 correction

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,13 +4,13 @@
  * @n: number int
  * @index: index of bit
  *
- * Return 1 success, -1 failure
+ * Return: 1 success, -1 failure
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n = (~(1UL << index) & *n);
+	*n &= ~(1 << index);
 	return (1);
 }
