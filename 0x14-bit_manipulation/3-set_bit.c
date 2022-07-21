064925828bfc7f1489c11ba75364b28a89daf3a0
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-21 14:49:48 -0700

    q3 correction

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,7 +6,7 @@
  *
  * Return: 1 success, -1 failure
  */
-int set_bit(unsiged long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
