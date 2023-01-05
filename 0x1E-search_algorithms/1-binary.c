65aa440f251688410d345fe24a8a2c139448dfdf
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-01-05 04:42:59 -0800

    q1 corection

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,13 +1,14 @@
 #include "search_algos.h"
 /**
-* pri_arr - prints array
-* array = array
-* start= start point
-* end = ending point
-* Return nothing
+* prar - prints array
+* @array: array
+* @start: start point
+* @end: ending point
+* Return: nothing
 **/
-void pri_arr(int *array, size_t start, size_t end)
+void prar(int *array, size_t start, size_t end)
 {
+
 	unsigned int i;
 
 	printf("Searching in array: ");
@@ -20,10 +21,10 @@ void pri_arr(int *array, size_t start, size_t end)
 
 /**
 * binary_search - a function that searches for a value in a
-* sorted array of integers using the Binary search algorithm
-* array: pointer to the first element of the array
-* size: is the number of elements in array
-* value: value to search for
+*sorted array of integers using the Binary search algorithm
+* @array: pointer to the first element of the array
+* @size: is the number of elements in array
+* @value: value to search for
 * Return: index OR -1
 **/
 int binary_search(int *array, size_t size, int value)
@@ -43,7 +44,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		if (left == 0 && right == 0)
 			return (-1);
-		pri_arr(array, left, right);
+		prar(array, left, right);
 		middle = (right + left) / 2;
 		if (array[middle] == value)
 			return (array[middle]);
@@ -57,4 +58,3 @@ int binary_search(int *array, size_t size, int value)
 		}
 	}
 	return (-1);
-}
