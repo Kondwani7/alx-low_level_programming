afc094f2801e5bcd28a21a66d85afa7a891ecce0
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-01-05 04:49:48 -0800

    q1 correction

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,14 +1,13 @@
 #include "search_algos.h"
 /**
-* prar - prints array
+* pri_arr: prints array
 * @array: array
 * @start: start point
 * @end: ending point
-* Return: nothing
+* Return nothing
 **/
-void prar(int *array, size_t start, size_t end)
+void pri_arr(int *array, size_t start, size_t end)
 {
-
 	unsigned int i;
 
 	printf("Searching in array: ");
@@ -21,7 +20,7 @@ void prar(int *array, size_t start, size_t end)
 
 /**
 * binary_search - a function that searches for a value in a
-*sorted array of integers using the Binary search algorithm
+* sorted array of integers using the Binary search algorithm
 * @array: pointer to the first element of the array
 * @size: is the number of elements in array
 * @value: value to search for
@@ -44,7 +43,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		if (left == 0 && right == 0)
 			return (-1);
-		prar(array, left, right);
+		pri_arr(array, left, right);
 		middle = (right + left) / 2;
 		if (array[middle] == value)
 			return (array[middle]);
@@ -58,3 +57,4 @@ int binary_search(int *array, size_t size, int value)
 		}
 	}
 	return (-1);
+}
